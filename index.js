9651a6b52ca22bfc07336d75f7feb43eed1d72cb
Rabiann <93538032+Rabiann@users.noreply.github.com>
2023-07-17 11:30:28 +0300

    .

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', function(req, res) {
+  let headers = req.headers
+  res.json({"ipaddress": headers.host, "language": headers['accept-language'], "software": headers['user-agent']})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
